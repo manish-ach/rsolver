d68ff59132bb533ffe38c00f13aa6c9397108295
Manish Acharya <manuacharya54@gmail.com>
2025-10-06 20:18:01 +0545

    added response

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("server listening on 127.0.0.1:8001");
     let mut buf = [0u8; 512];
 
+    let upstream_addr = "8.8.8.8:53";
+    let upstream = UdpSocket::bind("0.0.0.0:0").await?;
+
     loop {
-        let (len, addr) = socket.recv_from(&mut buf).await?;
-        println!("Recieved data addr is {addr} and the len is {len}");
+        let (len, client_addr) = socket.recv_from(&mut buf).await?;
+        println!("Recieved data addr is {client_addr} and the len is {len}");
 
         if let Err(e) = parse_dns_query(&buf[..len]) {
             eprintln!("failed to parse the packet: {e}");
         }
+
+        upstream.send_to(&buf[..len], upstream_addr).await?;
+        println!("query forwarded upstream to {upstream_addr}");
+
+        // RESPONSE
+        let mut response = [0u8; 512];
+        let (response_len, _) = upstream.recv_from(&mut response).await?;
+
+        if let Err(e) = parse_dns_query(&response[..response_len]) {
+            eprintln!("failed to parse the packet: {e}");
+        }
+
+        socket
+            .send_to(&response[..response_len], client_addr)
+            .await?;
+        println!("Sent response to {client_addr}\n");
     }
 }
 
@@ -43,49 +62,106 @@ fn parse_dns_query(data: &[u8]) -> Result<(), Box<dyn Error>> {
     // <!-----QUESTION------!>
     let mut offset = 12;
 
-    let (qname, new_offset) = read_qname(data, offset)?;
-    offset = new_offset;
+    for _ in 0..qdcount {
+        let (qname, new_offset) = read_qname(data, offset)?;
+        offset = new_offset;
 
-    if offset + 4 > data.len() {
-        return Err("Incomplete Question".into());
+        if offset + 4 > data.len() {
+            return Err("Incomplete Question".into());
+        }
+
+        let qtype = u16::from_be_bytes([data[offset], data[offset + 1]]);
+        let qclass = u16::from_be_bytes([data[offset + 2], data[offset + 3]]);
+
+        println!("--- QUESTION ---");
+        println!("Name: {}", qname);
+        println!("Type: {} ({:?})", qtype, record_type_name(qtype));
+        println!("Class: {}", qclass);
+        offset += 4;
     }
 
-    let qtype = u16::from_be_bytes([data[offset], data[offset + 1]]);
-    let qclass = u16::from_be_bytes([data[offset + 2], data[offset + 3]]);
+    // <!-----ANSWER------!>
+    for _ in 0..ancount {
+        let (name, new_offset) = read_qname(data, offset)?;
+        offset = new_offset;
 
-    println!("--- QUESTION ---");
-    println!("Name: {}", qname);
-    println!("Type: {} ({:?})", qtype, record_type_name(qtype));
-    println!("Class: {}", qclass);
+        if (offset + 10) > data.len() {
+            return Err("Incomplete answer record".into());
+        }
+
+        let rtype = u16::from_be_bytes([data[offset], data[offset + 1]]);
+        let _rclass = u16::from_be_bytes([data[offset + 2], data[offset + 3]]);
+        let ttl = u32::from_be_bytes([
+            data[offset + 4],
+            data[offset + 5],
+            data[offset + 6],
+            data[offset + 7],
+        ]);
+        let rdlength = u16::from_be_bytes([data[offset + 8], data[offset + 9]]) as usize;
+        offset += 10;
+
+        let rdata = &data[offset..offset + rdlength];
+        offset += rdlength;
+
+        println!(
+            "Answer: {name}  TTL={ttl}  TYPE={rtype} ({})",
+            record_type_name(rtype)
+        );
+
+        // If it's an A record (IPv4)
+        if rtype == 1 && rdlength == 4 {
+            println!(
+                "→ IPv4 Address: {}.{}.{}.{}",
+                rdata[0], rdata[1], rdata[2], rdata[3]
+            );
+        }
+    }
 
     Ok(())
 }
 
 fn read_qname(data: &[u8], mut offset: usize) -> Result<(String, usize), Box<dyn Error>> {
     let mut labels = Vec::new();
+    let mut jumped = false;
+    let mut jump_offset = 0usize;
 
     loop {
         if offset >= data.len() {
             return Err("QNAME extends beyond packet".into());
         }
 
-        let len = data[offset] as usize;
-        if len == 0 {
+        let len = data[offset];
+
+        // compression pointer
+        if len & 0xC0 == 0xC0 {
+            if offset + 1 >= data.len() {
+                return Err("Incomplete compression pointer".into());
+            }
+            let pointer = (((len & 0x3F) as u16) << 8) | data[offset + 1] as u16;
+
+            if !jumped {
+                jump_offset = offset + 2;
+                jumped = true;
+            }
+
+            offset = pointer as usize;
+            continue;
+        } else if len == 0 {
             offset += 1;
             break;
         }
 
         offset += 1;
-        if offset + len > data.len() {
+        if offset + (len as usize) > data.len() {
             return Err("label extends beyond the packet".into());
         }
 
-        let label = String::from_utf8(data[offset..offset + len].to_vec())?;
+        let label = String::from_utf8(data[offset..offset + (len as usize)].to_vec())?;
         labels.push(label);
-        offset += len;
+        offset += len as usize;
     }
     let name = labels.join(".");
-    Ok((name, offset))
+    Ok((name, if jumped { jump_offset } else { offset }))
 }
 
 fn record_type_name(qtype: u16) -> &'static str {
